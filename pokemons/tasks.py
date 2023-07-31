56fb4ad6d29d8332266374d2f7f7924491e027cd
Laura Cabantous-Taylor <laura.cabantous@gmail.com>
2023-07-31 22:20:11 +0100

    Implement scheduler for update_pokemon_data task

---
+++ pokemons/tasks.py
@@ -0,0 +1,9 @@
+from django.core.management import call_command
+from celery import shared_task
+
+@shared_task
+def update_pokemon_data():
+    """
+    Celery task to update or create Pokemon records using data from the PokeAPI.
+    """
+    call_command("update_pokemon_data")
